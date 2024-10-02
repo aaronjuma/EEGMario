12299a33adfead7673bc82c80f20b52d450b2cdb
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2024-10-02 17:00:04 -0400

    Survival Mode is Good

--- Game/Dev/HelloMarioFramework-main/Assets/Scripts/SurvivalController.cs
+++ Game/Dev/HelloMarioFramework-main/Assets/Scripts/SurvivalController.cs
@@ -11,40 +11,45 @@ public class SurvivalController : MonoBehaviour
     [SerializeField] private Transform goombaParent;
     [SerializeField] private Transform coinParent;
     [SerializeField] private float goombaChaseDistance = 10f;
-    
-    private int goombaCount = 10;
-    private int coinCount = 5;
+    [SerializeField] private int goombaCount = 15;
+    [SerializeField] private int coinCount = 5;
 
     // Start is called before the first frame update
     void Start() {
 
         // Spawn Goombas
         for (int i = 0; i < goombaCount; ++i){
-            generateGoomba();
+            GenerateGoomba();
         }
         Debug.Log(goombaParent.childCount);
 
         // Spawn coins
         for (int i = 0; i < coinCount; ++i) {
-            generateCoin();
+            GenerateCoin();
         }
     }
 
     // Update is called once per frame
     void Update() {
         if (goombaParent.childCount < goombaCount) {
-            generateGoomba();
+            GenerateGoomba();
         }
         if (coinParent.childCount < coinCount) {
-            generateCoin();
+            GenerateCoin();
         }
     }
 
 
 
-    private void generateGoomba() {
+    private void GenerateGoomba() {
         float x = Random.Range(-19, 19);
         float z = Random.Range(-19, 19);
+
+        //Prevent goombas from spawning near the
+        while (Mathf.Abs(x) < 5 || Mathf.Abs(z) < 5) {
+            x = Random.Range(-19, 19);
+            z = Random.Range(-19, 19);
+        }
         Vector3 pos = new Vector3(x, 3, z);
         Quaternion currentRotation = new Quaternion();
         currentRotation.eulerAngles = new Vector3(0, 90, 0);
@@ -52,7 +57,7 @@ public class SurvivalController : MonoBehaviour
         goomba.GetComponent<Enemy>().chaseDistance = goombaChaseDistance;
     }
 
-    private void generateCoin() {
+    private void GenerateCoin() {
         float x = Random.Range(-19, 19);
         float z = Random.Range(-19, 19);
         Vector3 pos = new Vector3(x, 4, z);
