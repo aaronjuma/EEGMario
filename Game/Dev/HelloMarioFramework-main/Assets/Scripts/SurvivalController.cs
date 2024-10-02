7c75dbb5af212c809eade479ef9096f4a98ca6c3
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2024-10-02 15:38:55 -0400

    Enemy and Coin spawning works well

--- Game/Dev/HelloMarioFramework-main/Assets/Scripts/SurvivalController.cs
+++ Game/Dev/HelloMarioFramework-main/Assets/Scripts/SurvivalController.cs
@@ -9,25 +9,53 @@ public class SurvivalController : MonoBehaviour
     [SerializeField] private GameObject goombaPrefab;
     [SerializeField] private GameObject coinPrefab;
     [SerializeField] private Transform goombaParent;
+    [SerializeField] private Transform coinParent;
     
+    private int goombaCount = 10;
+    private int coinCount = 5;
+
     // Start is called before the first frame update
     void Start() {
 
         // Spawn Goombas
-        for (int i = 0; i < 10; ++i){
-            float x = Random.Range(-19, 19);
-            float z = Random.Range(-19, 19);
-            Vector3 pos = new Vector3(x, 3, z);
-            Quaternion currentRotation = new Quaternion();
-            currentRotation.eulerAngles = new Vector3(0, 90, 0);
-            Instantiate(goombaPrefab, pos, currentRotation, goombaParent);
-            
+        for (int i = 0; i < goombaCount; ++i){
+            generateGoomba();
         }
         Debug.Log(goombaParent.childCount);
+
+        // Spawn coins
+        for (int i = 0; i < coinCount; ++i) {
+            generateCoin();
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (goombaParent.childCount < goombaCount) {
+            generateGoomba();
+        }
+        if (coinParent.childCount < coinCount) {
+            generateCoin();
+        }
+    }
+
+
+
+    private void generateGoomba() {
+        float x = Random.Range(-19, 19);
+        float z = Random.Range(-19, 19);
+        Vector3 pos = new Vector3(x, 3, z);
+        Quaternion currentRotation = new Quaternion();
+        currentRotation.eulerAngles = new Vector3(0, 90, 0);
+        Instantiate(goombaPrefab, pos, currentRotation, goombaParent);
+    }
+
+    private void generateCoin() {
+        float x = Random.Range(-19, 19);
+        float z = Random.Range(-19, 19);
+        Vector3 pos = new Vector3(x, 4, z);
+        Quaternion currentRotation = new Quaternion();
+        currentRotation.eulerAngles = new Vector3(0, 90, 0);
+        Instantiate(coinPrefab, pos, currentRotation, coinParent);
     }
 }
